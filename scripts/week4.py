565bd91d5b0a444caf0bdf923c9ee8274bbd83d8
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2025-09-28 17:22:15 -0700

    Complete week 4

--- scripts/week4.py
+++ scripts/week4.py
@@ -31,22 +31,46 @@ import numpy as np
 
 # 3. Compute eigenvalues and eigenvectors of `[[4,2],[2,3]]`.  
 
-C = np.array([[4, 2], [2, 3]])
+# c = np.array([[4, 2], [2, 3]])
 
-print(np.linalg.eig(C))
+# print(np.linalg.eig(C))
 
-c_eigenvals, c_eigenvects = np.linalg.eig(C)[0], np.linalg.eig(C)[1]
-
-
-
-# 4. Verify that `A @ v = λv` holds for one eigenpair.  
+# c_eigenvals, c_eigenvects = np.linalg.eig(c)[0], np.linalg.eig(c)[1]
 
 
-print(C@c_eigenvects[:, 0])
-print(c_eigenvals[0]*c_eigenvects[:, 0])
+
+# # 4. Verify that `A @ v = λv` holds for one eigenpair.  
+
+
+# print(C@c_eigenvects[:, 0])
+# print(c_eigenvals[0]*c_eigenvects[:, 0])
 
 # **Stretch Challenges**
 # - Create a random symmetric 3×3 matrix. Compute its eigenvalues and confirm they’re real.  
+
+d = np.random.randint(1, 10, 9).reshape((3, 3))
+
+e = np.linalg.matrix_transpose(d) + d
+
+def check_eigenvals (matrix):
+    vals, vects = np.linalg.eig(matrix)[0], np.linalg.eig(matrix)[1]
+    print(matrix@vects[:,0])
+    print(vals[0]*vects[:,0])
+    return np.allclose(matrix@vects[:, 0].reshape(3), vals[0]*vects[:, 0].reshape(3))
+
+print(check_eigenvals(e))
+
+
+
+
+
 # - Compute the rank of a random 4×4 matrix. Explain what it means.  
+
+f = np.random.randint(1, 30, 16).reshape((4, 4))
+
+print(np.linalg.matrix_rank(f))
+
+
+# rank is how many unique solns, or populated rows in rref
 
 # ---
