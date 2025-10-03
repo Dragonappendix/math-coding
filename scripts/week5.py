9f7e1c21eda189595363ae9c466d28d2f225e831
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2025-10-02 22:08:11 -0700

    Start week 5

--- scripts/week5.py
+++ scripts/week5.py
@@ -6,6 +6,13 @@ import numpy as np
 
 # **Exercises**
 # 1. Generate `x = [1,2,3,4,5]`, `y = [2,4,6,8,10]`. Fit a line using `np.polyfit`.  
+
+x = np.array([1, 2, 3, 4, 5])
+
+y = np.array([2, 4, 6, 8, 10])
+
+fitted_line = np.polyfit(x, y, 1)
+print(fitted_line)
 # 2. Implement linear regression manually using the normal equation:  
 
 # β = (XᵀX)⁻¹Xᵀy
